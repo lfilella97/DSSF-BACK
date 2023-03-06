9dbb217bc04b650de2df53fa62c79dbe11208ef5
lfilella97 <lfilella97@gmail.com>
2023-03-06 19:47:15 +0100

    Install Jest, ts-jest, webcompat-resolver and its types

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["src/**/*.test.ts"],
+  resolver: "jest-ts-webcompat-resolver",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "src/**/*.test.ts",
+    "!src/index.ts",
+    "!src/loadEnvironment.ts",
+    "!src/server/startServer.ts",
+    "!src/server/index.ts",
+    "!src/database/connectDatabase.ts",
+    "!src/server/app.ts",
+  ],
+};
