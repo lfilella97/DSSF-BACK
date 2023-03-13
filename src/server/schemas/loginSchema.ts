722b0ffd881df71b2a43e5636dcb6ac0245cf935
lfilella97 <lfilella97@gmail.com>
2023-03-13 09:56:01 +0100

    Modify login validation, just required

--- src/server/schemas/loginSchema.ts
+++ src/server/schemas/loginSchema.ts
@@ -2,8 +2,8 @@ import { Joi } from "express-validation";
 
 const loginSchema = {
   body: Joi.object({
-    userName: Joi.string().min(3).max(14).required(),
-    password: Joi.string().min(6).max(17).required(),
+    userName: Joi.string().required(),
+    password: Joi.string().required(),
   }),
 };
 
